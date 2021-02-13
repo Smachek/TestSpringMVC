1cd2af7867a4524d4ec4dc5313208850da5aa1c8
Sergey Smachek <SergeySmachek@gmail.com>
2021-02-13 21:28:04 +0300

    Add Thymeleaf

--- src/main/java/com/test/springmvc/HelloController.java
+++ src/main/java/com/test/springmvc/HelloController.java
@@ -7,14 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
 
     // Пользователь вводит в адресной строке браузера <адрес сервера>/ и будет попадать этот метод
-    // В ответ мы возвращаем пользователю представление /WEB-INF/views/index.jsp.
+    // В ответ мы возвращаем пользователю представление /WEB-INF/views/index.html.
     @GetMapping("/")
     public String index() {
         return "index";
     }
 
     // Пользователь вводит в адресной строке браузера <адрес сервера>/hello и будет попадать этот метод
-    // В ответ мы возвращаем пользователю представление /WEB-INF/views/hello_world.jsp.
+    // В ответ мы возвращаем пользователю представление /WEB-INF/views/hello_world.html.
     @GetMapping("/hello")
     public String hello() {
         return "hello_world";
